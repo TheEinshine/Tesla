a52bc1bf35a9b38eebbe6bf5066cf1a7f9cd90f0
Einshine <theeinshine3301@gmail.com>
2023-05-15 13:08:49 +0530

    update UI

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,13 +3,19 @@ import '../assets/css/Landing.css'
 
 const LandingPage = () => {
   return (
-    <>
+    <div className='videoContainer'>
         <div className='landingDiv'>
             <div className='introVid'>
-                <video autoPlay muted  loop src="https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Homepage-Test_Drive-NA-Desktop.mp4" ></video>
+                <video id='teslaVideo' autoPlay muted  loop src="https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Homepage-Test_Drive-NA-Desktop.mp4" ></video>
+                <div className="overlay">
+                    <div className="overlayText">
+                        <h1>Model S</h1>
+                        <h4>Order Online for <span>Touchless Delivery</span></h4>
+                        </div>
+                </div>
             </div>
         </div>
-    </>
+    </div>
   )
 }
 
